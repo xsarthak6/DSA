12889afa85a1b82b7063a8dd475a29b9061fd2b2
xsarthak6 <sarthakjais612@gmail.com>
2026-09-18 23:21:19 +0530

    Time: 0 ms (100.00%) | Memory: 9.1 MB (95.40%) - LeetSync

--- 389-find-the-difference/find-the-difference.cpp
+++ 389-find-the-difference/find-the-difference.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
-    char findTheDifference(string s, string t) {
-        char ans=0;
-        for(int i=0;i<s.size();i++)
-        {
-            ans^=s[i];
-            ans^=t[i];
+    char findTheDifference(std::string& s, std::string& t) {
+        // Intuition: char range is fixed to lowercase letters, use frequency array instead of hashmap
+        std::vector<int> freq(26, 0);
+        for (char c : s) {
+            freq[c - 'a']++;
         }
-        ans^=t[s.size()];
-        return ans;
+        for (char c : t) {
+            freq[c - 'a']--;
+            if (freq[c - 'a'] < 0) return c;
+        }
+        return 'a';
     }
 };
